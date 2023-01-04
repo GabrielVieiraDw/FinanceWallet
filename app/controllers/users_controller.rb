7b10de878939a49adebeea9cfeeb48973479d226
GabrielVieiraDw <gabrielvieiradw@gmail.com>
2023-01-04 09:54:52 -0300

    Create users and stock associations, Display user stock list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
   def my_wallet
+    @tracked_stocks = current_user.stocks
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.new_stock_search(ticker)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:iex_api_key],
